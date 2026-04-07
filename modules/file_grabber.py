7af32e3161f4875a84fe578beeee4060b62dc7fd
Daniele Dellavalle <187041599+dllvll@users.noreply.github.com>
2026-04-07 14:00:50 +0200

    refactor file_grabber.py

--- modules/file_grabber.py
+++ modules/file_grabber.py
@@ -1,27 +1,12 @@
 import requests
 
-def get_robots_txt(base_url):
-    """Recupera il file robots.txt di un sito."""
-    url = f"{base_url}/robots.txt"
-    robots = requests.get(url, timeout=10)
+def get_file(base_url, path):
+    """Recupera un file da un URL specificato."""
+    url = f"{base_url}/{path}"
+    file = requests.get(url, timeout=10)
 
-    if robots.status_code == 404:
+    if file.status_code == 404:
         return None
     
-    robots.raise_for_status()
-    return robots.text
-
-def get_security_txt(base_url):
-    """Recupera il file security.txt di un sito."""
-    url = f"{base_url}/security.txt"
-    security = requests.get(url, timeout=10)
-    
-    if security.status_code != 200:
-        url = f"{base_url}/.well-known/security.txt"
-        security = requests.get(url, timeout=10)
-    
-    if security.status_code == 404:
-        return None
-
-    security.raise_for_status()
-    return security.text
+    file.raise_for_status()
+    return file.text
