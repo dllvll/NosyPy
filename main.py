8ea1f746968552fb40db1bff802b4ef358ca48a7
Daniele Dellavalle <187041599+dllvll@users.noreply.github.com>
2026-04-07 14:06:35 +0200

    translate main.py from Italian to English

--- main.py
+++ main.py
@@ -18,40 +18,41 @@ def main():
     parsed = urlsplit(parser.parse_args().url)
 
     if parsed.scheme not in ["http", "https"]:
-        sys.exit("Errore: inserisci un URL valido con http:// o https://.")
+        sys.exit("Error: please enter a valid URL with http:// or https://.")
 
     base_url = f"{parsed.scheme}://{parsed.netloc}"
     print(f"\t[ Host: {parsed.netloc} ]\n")
+
+    # ROBOTS
+    try:
+        robots = file_grabber.get_file(base_url, "robots.txt")
+
+        if robots is None:
+            console.print_warning("robots.txt", "Robots Exclusion Protocol: robots.txt not found on this host.")
+        else:
+            utils.save_file(f"{parsed.netloc}_robots.txt", robots)
+            console.print_success("robots.txt", f"Robots Exclusion Protocol: robots.txt saved as {parsed.netloc}_robots.txt")
+
+    except requests.exceptions.ConnectionError:
+        console.print_error("robots.txt", "Error: host is unreachable.")
+    except requests.exceptions.HTTPError as e:
+        console.print_error("robots.txt", "HTTP Error: " + e.args[0])
 
     # SECURITY
     try:
         security = file_grabber.get_security_txt(base_url)
 
         if security is None:
-            console.print_warning("security.txt", "Il file security.txt non è presente sul sito.")
+            console.print_warning("security.txt", "security.txt not found on this host.")
         else:
             utils.save_file(f"{parsed.netloc}_security.txt", security)
-            console.print_success("security.txt", f"File security.txt salvato come {parsed.netloc}_security.txt")
+            console.print_success("security.txt", f"security.txt saved as {parsed.netloc}_security.txt")
 
     except requests.exceptions.ConnectionError:
-        console.print_error("security.txt", "Errore: il dominio non è raggiungibile.")
+        console.print_error("security.txt", "Error: host is unreachable.")
     except requests.exceptions.HTTPError as e:
-        console.print_error("security.txt", "Errore HTTP: " + e.args[0])
+        console.print_error("security.txt", "HTTP Error: " + e.args[0])
 
-    # ROBOTS
-    try:
-        robots = file_grabber.get_robots_txt(base_url)
-
-        if robots is None:
-            console.print_warning("robots.txt", "Il file robots.txt non è presente sul sito.")
-        else:
-            utils.save_file(f"{parsed.netloc}_robots.txt", robots)
-            console.print_success("robots.txt", f"File robots.txt salvato come {parsed.netloc}_robots.txt")
-
-    except requests.exceptions.ConnectionError:
-        console.print_error("robots.txt", "Errore: il dominio non è raggiungibile.")
-    except requests.exceptions.HTTPError as e:
-        console.print_error("robots.txt", "Errore HTTP: " + e.args[0])
 
     # WHOIS
     try:
@@ -60,20 +61,20 @@ def main():
         console.print_table(w, "WHOIS\n")
         print()
     except whois.exceptions.WhoisDomainNotFoundError:
-        console.print_error("WHOIS", "Errore: il dominio non è stato trovato.")
+        console.print_error("WHOIS", "Error: the domain was not found.")
     except whois.exceptions.PywhoisError as e:
-        console.print_error("WHOIS", "Errore WHOIS: " + e.args[0])
+        console.print_error("WHOIS", "WHOIS Error: " + e.args[0])
 
     # HTTP Headers
     try:
         headers = http_headers.get_headers(base_url)
-        console.print_success("HTTP Headers", "Intestazioni HTTP recuperate:\n")
+        console.print_success("HTTP Headers", "HTTP Headers:\n")
         console.print_table(headers, "HTTP Headers\n")
         print()
     except requests.exceptions.ConnectionError:
-        console.print_error("HTTP Headers", "Errore: il dominio non è raggiungibile.")
+        console.print_error("HTTP Headers", "Error: host is unreachable.")
     except requests.exceptions.HTTPError as e:
-        console.print_error("HTTP Headers", "Errore HTTP: " + e.args[0])
+        console.print_error("HTTP Headers", "HTTP Error: " + e.args[0])
 
 if __name__ == "__main__":
     main()
