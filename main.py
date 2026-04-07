111555743e3bce127517de2eba8147de7278b247
Daniele Dellavalle <187041599+dllvll@users.noreply.github.com>
2026-04-07 15:11:03 +0200

    replace robots.txt and security.txt with a well-knowns prober

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from rich import print
 from modules import console
 from modules import http_headers
 from modules import whois_lookup
-from modules import utils
-from modules import file_grabber
+from modules import page_prober
 from urllib.parse import urlsplit
 
 def main():
@@ -23,36 +22,21 @@ def main():
     base_url = f"{parsed.scheme}://{parsed.netloc}"
     print(f"\t[ Host: {parsed.netloc} ]\n")
 
-    # ROBOTS
-    try:
-        robots = file_grabber.get_file(base_url, "robots.txt")
+    # PROBE WELL_KNOWNS.TXT
+    with open("well_knowns.txt", "r") as file:
+        well_knowns = file.readlines()
 
-        if robots is None:
-            console.print_warning("robots.txt", "File not found on this host.")
-        else:
-            utils.save_file(f"{parsed.netloc}_robots.txt", robots)
-            console.print_success("robots.txt", f"File saved as {parsed.netloc}_robots.txt")
-
-    except requests.exceptions.ConnectionError:
-        console.print_error("robots.txt", "Error: host is unreachable.")
-    except requests.exceptions.HTTPError as e:
-        console.print_error("robots.txt", "HTTP Error: " + e.args[0])
-
-    # SECURITY
-    try:
-        security = file_grabber.get_file(base_url, "security.txt")
-
-        if security is None:
-            console.print_warning("security.txt", "File not found on this host.")
-        else:
-            utils.save_file(f"{parsed.netloc}_security.txt", security)
-            console.print_success("security.txt", f"File saved as {parsed.netloc}_security.txt")
-
-    except requests.exceptions.ConnectionError:
-        console.print_error("security.txt", "Error: host is unreachable.")
-    except requests.exceptions.HTTPError as e:
-        console.print_error("security.txt", "HTTP Error: " + e.args[0])
-
+    for well_known in well_knowns:
+        url = f"{base_url}/{well_known.strip()}"
+        try:
+            status_code = page_prober.probe_url(url)
+            if status_code == 200:
+                console.print_success("(well-known)", f"Found {well_known.strip()} at {url}")
+            else:
+                console.print_warning("(well-known)", f"{well_known.strip()} not found (Status code: {status_code})",
+                )
+        except requests.exceptions.RequestException as e:
+            console.print_error("(well-known)", f"Error probing {well_known.strip()}: {e}")
 
     # WHOIS
     try:
