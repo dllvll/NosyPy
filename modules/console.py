b87986595caa212d8ad2f777e17bcc71c0fd98a1
Daniele Dellavalle <187041599+dllvll@users.noreply.github.com>
2026-04-07 13:46:30 +0200

    remove redundant table headers

--- modules/console.py
+++ modules/console.py
@@ -15,9 +15,9 @@ def print_banner():
 
 def print_table(tableData, tableTitle):
     """Stampa una tabella di informazioni."""
-    table = Table()
-    table.add_column("Campo")
-    table.add_column("Valore")
+    table = Table(show_header=False)
+    table.add_column()
+    table.add_column()
 
     for key, value in tableData.items():
         table.add_row(key, str(value))
