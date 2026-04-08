d3747860d2175131a7344212fa7ba6a7ca7640cd
Daniele Dellavalle <187041599+dllvll@users.noreply.github.com>
2026-04-08 12:45:34 +0200

    implement ip lookup and geo lookup

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import requests
 import argparse
 import whois
 import sys
+import socket
 from rich import print
 from modules import console
 from modules import http_headers
 from modules import whois_lookup
 from modules import page_prober
+from modules import ip_lookup
+from modules import geo_lookup
 from urllib.parse import urlsplit
 from rich.progress import track
 
@@ -24,7 +27,26 @@ def main():
     base_url = f"{parsed.scheme}://{parsed.netloc}"
     print(f"\t[ Host: {parsed.netloc} ]\n")
 
+    # IP LOOKUP
+    try:
+        ip_address = ip_lookup.get_ip_address(parsed.netloc)
+        console.print_success("ip_lookup", f"IP address found: {ip_address}")
+    except socket.gaierror:
+        console.print_error("ip_lookup", "Error: unable to resolve the hostname.")
+
+    # IP GEOLOCATION
+    print()
+    try:
+        geo_lookup.get_geolocation(ip_address)
+        console.print_success("geo_lookup", f"Geolocation information found for {ip_address}:")
+        console.print_table(geo_lookup.get_geolocation(ip_address), "Geolocation")
+    except requests.exceptions.ConnectionError:
+        console.print_error("HTTP Headers", "Error: host is unreachable.")
+    except requests.exceptions.HTTPError as e:
+        console.print_error("HTTP Headers", "HTTP Error: " + e.args[0])
+
     # WELL-KNOWNS PROBING
+    print()
     with open("well_knowns.txt", "r") as file:
         well_knowns = file.readlines()
 
@@ -44,22 +66,22 @@ def main():
             )
 
     # WHOIS
+    print()
     try:
         w = whois_lookup.get_whois(parsed.netloc)
         console.print_success("whois", "WHOIS Information found:")
         console.print_table(w, "WHOIS")
-        print()
     except whois.exceptions.WhoisDomainNotFoundError:
         console.print_error("WHOIS", "Error: the domain was not found.")
     except whois.exceptions.PywhoisError as e:
         console.print_error("WHOIS", "WHOIS Error: " + e.args[0])
 
     # HTTP Headers
+    print()
     try:
         headers = http_headers.get_headers(base_url)
         console.print_success("http_headers", "HTTP Headers found:")
         console.print_table(headers, "HTTP Headers")
-        print()
     except requests.exceptions.ConnectionError:
         console.print_error("HTTP Headers", "Error: host is unreachable.")
     except requests.exceptions.HTTPError as e:
