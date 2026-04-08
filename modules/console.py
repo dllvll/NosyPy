7a81aeaf9edbc9428e5cf3172d8e558b2e1162dc
Daniele Dellavalle <187041599+dllvll@users.noreply.github.com>
2026-04-08 18:48:35 +0200

    type hint console.py functions

--- modules/console.py
+++ modules/console.py
@@ -1,7 +1,7 @@
 from rich.table import Table
 from rich import print
 
-def print_banner():
+def print_banner() -> None:
     """Print NosyPy's ASCII art banner."""
     banner = r"""
     ▄▄▄    ▄▄▄                   ▄▄▄▄▄▄▄         
@@ -24,14 +24,14 @@ def print_table(tableData, tableTitle):
         table.add_row(key, str(value))
     print(table)
 
-def print_success(scope, message):
+def print_success(scope: str, message: str) -> None:
     """Print a success message."""
     print(f"({scope}) {message}")
 
-def print_error(scope, message):
+def print_error(scope: str, message: str) -> None:
     """Print an error message."""
     print(f"({scope}) {message}")
 
-def print_warning(scope, message):
+def print_warning(scope: str, message: str) -> None:
     """Print a warning message."""
     print(f"({scope}) {message}")
