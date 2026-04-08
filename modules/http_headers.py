c1c8c5512b5708863e75ecfa6332df956447a3fc
Daniele Dellavalle <187041599+dllvll@users.noreply.github.com>
2026-04-08 18:25:26 +0200

    translate http_headers.py's docstring from Italian to English

--- modules/http_headers.py
+++ modules/http_headers.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_headers(domain):
-    """Recupera gli headers HTTP di un dominio."""
+    """Get HTTP headers for a given domain."""
     r = requests.get(domain, timeout=10)
     r.raise_for_status()
     return r.headers
