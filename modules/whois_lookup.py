df1bf65b8d209c42d39e7d61d3d1a449e3aa15d5
dllvll <187041599+dllvll@users.noreply.github.com>
2026-04-30 11:12:08 +0200

    type hint get_whois and update its docstring accordingly

--- modules/whois_lookup.py
+++ modules/whois_lookup.py
@@ -1,6 +1,14 @@
 import whois
 
 
-def get_whois(domain):
-    """Get WHOIS information for a given domain."""
+def get_whois(domain: str) -> dict:
+    """Gets/ WHOIS information for a given domain.
+
+    Args:
+        domain (str): The domain to look up.
+
+    Returns:
+        dict: A dictionary containing the WHOIS information for the domain.
+    """
+    
     return whois.whois(domain)
