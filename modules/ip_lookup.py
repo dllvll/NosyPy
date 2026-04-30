efb1cb58e3df23a8c07fb37ec9fe76fbd3265690
dllvll <187041599+dllvll@users.noreply.github.com>
2026-04-30 11:09:33 +0200

    type hint get_ip_address and update its docstring accordingly

--- modules/ip_lookup.py
+++ modules/ip_lookup.py
@@ -1,7 +1,15 @@
 import socket
 
 
-def get_ip_address(hostname):
-    """Get the IP address of a given hostname."""
+def get_ip_address(hostname: str) -> str:
+    """Gets the IP address of a given hostname.
+    
+    Args:
+        hostname: The hostname for which to fetch the IP address.
+
+    Returns:
+        The IP address as a string.
+    """
+
     ip_address = socket.gethostbyname(hostname)
     return ip_address
