a30c2c81f567d547d03c6aee5dfc3acff778cf6b
dllvll <187041599+dllvll@users.noreply.github.com>
2026-05-02 10:00:23 +0200

    implement config.timeout in the get_file function of file_grabber.py

--- modules/file_grabber.py
+++ modules/file_grabber.py
@@ -1,18 +1,21 @@
 import requests
 
+from NosyPy.config import Config
 
-def get_file(url: str) -> str | None:
+
+def get_file(url: str, config: Config) -> str | None:
     """Downloads a file from a given URL.
 
     Args:
         url: The URL of the file to download.
+        config: The configuration object containing timeout and delay settings.
 
     Returns:
         The content of the downloaded file, or None if the file was not found.
     """
     file = requests.get(
         url,
-        timeout=10,
+        timeout=config.timeout,
         headers={
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:149.0) Gecko/20100101 Firefox/149.0"
         },
